2f422f17cd35e345713b7d6c56418e153e5c8d69
EdelbertoReyes <Edelberto.Reyes@marcusmillichap.com>
2025-02-06 17:21:17 -0500

    Delete unnecessary swagger config.

--- EventApi.WebApi/Startup.cs
+++ EventApi.WebApi/Startup.cs
@@ -38,7 +38,7 @@ public static class Startup
 
         //DB contex adding
         services.AddDbContext<EventApiDbContext>(o => 
-        o.UseSqlServer(config.GetValue<string>("ConnectionStrings:DatabaseConnection"),
+         o.UseSqlServer(config.GetValue<string>("ConnectionStrings:DatabaseConnection"),
          b => b.MigrationsAssembly(typeof(EventApiDbContext).Assembly.FullName)));
 
         //Include repositories.
@@ -46,57 +46,7 @@ public static class Startup
         services.AddScoped<IUserRepository, UserRepository>();
 
         //TODO: Add Swagger
-        services.AddSwaggerGen(options =>
-        {
-            // Swagger gets confused by classes with the same name in different namespaces;
-            // this tells it to use the fully qualifed name.
-            options.CustomSchemaIds(schemaType => schemaType.FullName);
-
-            // Adds documentation to endpoints based on C# type attribution in the generated XML file.
-            // (The generated file is named in the csproj project definition and is generated with builds.)
-            options.IncludeXmlComments("Properties/EventApi.xml", true);
-
-            // Including details from the project readme markdown file in swagger.
-            var assembly = Assembly.GetExecutingAssembly();
-            using var readmeResource = assembly.GetManifestResourceStream("EventApi.WebApi.readme.md");
-            using var readmeStream = new StreamReader(readmeResource);
-
-            // Creates documentation for the version.
-            // (Ideally this would loop for versions.)
-            options.SwaggerDoc("v1", new OpenApiInfo
-            {
-                Title = "Event Api",
-                Version = "v1",
-                Description = readmeStream.ReadToEnd()
-            });
-
-            // Add security awareness for bearer tokens
-            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            {
-                Description = "Auth header value: Bearer <your-JWT-token>",
-                In = ParameterLocation.Header,
-                Name = "Authorization",
-                Scheme = "bearer",
-                Type = SecuritySchemeType.ApiKey
-            });
-
-            options.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        }
-                    },
-                    new List<string>()
-                }
-            });
-
-            options.EnableAnnotations();
-        });
+        services.AddSwaggerGen();
 
         return builder;
     }
